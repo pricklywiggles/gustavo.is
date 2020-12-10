b98a126282a0ae7a1861b1523c7435b25ddba823
Gustavo Gallegos <meaning.app>
2020-12-09 16:51:32 -0800

    tightening the main page copy and format

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,43 +39,51 @@ export default function Home() {
               <ProfilePhoto />
             </div>
           </div>
-          <p className="mt-3 text-base sm:text-lg md:mt-5 md:text-xl">
-            I&lsquo;m a developer living in Los Angeles with my wonderful dog
-            Luna, specializing in Javascript, React and GraphQL. I&lsquo;ve
-            loved technology since I was very young. I remember when I was a 15
-            year kid in Guadalajara I somehow convinced my parents to buy me an
-            IBM PS/2 desktop so I could try some Basic and C++ programming. A
-            technician came to your house to install it, as was common back
-            then, and I stood right next to him memorizing the commands he typed
-            into the DOS prompt. By the time he left I thought to myself
-            &quot;That didn&lsquo;t look so hard&quot;, so I sat down and typed
-            my very first computer command:
-            <code className="block text-sm my-4 mx-auto font-code w-max px-4 py-2 rounded-md p- bg-gray-800 text-gray-200 uppercase">
-              format c:
-              <span className="r animate-pulse">_</span>
-            </code>{" "}
-            thus beginning a long sequence of learning opportunities up to this
-            day. By the time I went to high school I had taught myself C and
-            C++. I soon left Mexico for the US to get a CS degree and later
-            began my career as a developer at Microsoft before moving to the Bay
-            Area to work helping startups grow several times their size,
-            including a stint as Director of QA at Max Levchin&lsquo;s Slide and
-            later as the Technical Program Manager for the Health division of
-            Jawbone. It was rewarding work, but I missed the feeling of finding
-            elegant solutions to problems through code. So two years ago I
-            decided to jump back into development by co-founding the company
-            behind ponder.to, a group blogging platform we recently had to
-            shutter. As difficult as it was to get back to speed after not
-            coding for a long time, I loved every minute of it. Using code to
-            bring an experience to life is a joy, no matter how many{" "}
-            <span className="pl-2 text-sm pr-2 font-semibold font-code uppercase">
-              format c:
-            </span>
-            moments you have along the way. I love working on my own things but
-            I am eager to find my tribe, people I can learn from and with whom I
-            can make something meaningful and continue this learning journey.
-            Does that sound like you? drop me a line below!
-          </p>
+          <div className="mt-3 text-base sm:text-lg md:mt-5 md:text-xl">
+            <p>
+              I&lsquo;m a developer living in Los Angeles with my wonderful dog
+              Luna, specializing in Javascript, React and GraphQL. I&lsquo;ve
+              loved technology since I was very young. I remember when I was a
+              15 year kid in Guadalajara I somehow convinced my parents to buy
+              me an IBM PS/2 desktop so I could try some Basic and C++
+              programming. A technician came to your house to install it, as was
+              common back then, and I stood right next to him memorizing the
+              commands he typed into the DOS prompt. By the time he left I
+              thought to myself &quot;That didn&lsquo;t look so hard&quot;, so I
+              sat down and typed my very first computer command...
+              <code className="block mx-auto text-sm my-4  font-code w-max px-4 py-1 rounded-md p- bg-gray-800 text-gray-200 uppercase">
+                C> format c:
+                <span className="r animate-pulse">_</span>
+              </code>
+            </p>
+            <p>
+              And thus began a long sequence of learning opportunities that
+              continue to this day. By the time I went to high school I had
+              taught myself C and C++. I soon left Mexico for the US to get a CS
+              degree and later began my career as a developer at Microsoft
+              before moving to the Bay Area to work helping startups grow
+              several times their size, including a stint as Director of QA at
+              Max Levchin&lsquo;s Slide and later as the Technical Program
+              Manager for the Health division of Jawbone. It was rewarding work,
+              but I missed the feeling of finding elegant solutions to problems
+              through code.
+            </p>
+            <p className="mt-4">
+              So two years ago I decided to jump back into development by
+              co-founding the company behind ponder.to, a group blogging
+              platform we recently had to shutter. As difficult as it was to get
+              back to speed after not coding for a long time, I loved every
+              minute of it. Using code to bring an experience to life is a joy,
+              no matter how many{" "}
+              <span className="pl-2 text-sm pr-2 font-semibold font-code uppercase">
+                format c:
+              </span>
+              moments you have along the way. I love working on my own things
+              but I am eager to find my tribe, people I can learn from and with
+              whom I can make something meaningful and continue this learning
+              journey. Does that sound like you? drop me a line below!
+            </p>
+          </div>
         </div>
       </div>
       <div>
