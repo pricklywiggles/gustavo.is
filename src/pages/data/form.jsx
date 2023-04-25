32748a0a7b4f4e0dbc6c29a9690543efb2b2a0ca
Gustavo Gallegos <goose@fractal.ly>
2023-04-25 09:54:57 -0700

    leave as dev

--- src/pages/data/form.jsx
+++ src/pages/data/form.jsx
@@ -3,15 +3,15 @@ import Script from 'next/script';
 export default function DataForm() {
   return (
     <>
-      <Script data-env="staging" src="https://demo.tartle.co/scripts/tartle-widget.js" />
+      <Script data-env="development" src="http://localhost:3000/scripts/tartle-widget.js" />
       <div className="mt-28 w-screen">
         <div className="w-min mx-auto">
           <div className="">
             <div
               id="tartle-container"
               className="rounded-md overflow-hidden"
-              data-packet-id="R1vaQ7Bebmx8N"
-              data-referral-code="a4fc47faffd3cce77ea6df565fcbeaf6"
+              data-packet-id="jVYa25pL4v1r3"
+              data-referral-code="2d454d17021543451c2747d2b9fdcd7f"
             />
           </div>
         </div>
