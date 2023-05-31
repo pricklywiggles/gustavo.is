59e24804d71314a232a61b3fa02a519e3d621936
Gustavo Gallegos <goose@fractal.ly>
2023-05-31 13:14:28 -0700

    staging

--- src/pages/data/form.jsx
+++ src/pages/data/form.jsx
@@ -3,14 +3,28 @@ import Script from 'next/script';
 export default function DataForm() {
   return (
     <>
-      <Script data-env="staging" src="https://demo.tartle.co/scripts/tartle-widget.js" />
+      <Script
+        id="tartle-script"
+        data-env="staging"
+        src="https://demo.tartle.co/scripts/tartle-widget.js"
+      />
+      {/* <Script
+        id="tartle-script"
+        data-env="http://localhost:3000"
+        src="http://localhost:3000/scripts/tartle-widget.js"
+      /> */}
+      {/* <Script
+        id="tartle-script"
+        // data-env="http://localhost:3000"
+        src="http://source.tartle.co/scripts/tartle-widget.js"
+      /> */}
       <Script id="tartle-pubkey">
         {`
           console.log("Setting up event listener")
           window.addEventListener('message', function (event) {
             console.log(event.data)
-            if (event.data.action === 'tt-pubkey') {
-              console.log('Public key:', event.data.pubkey);
+            if (event.data.type === 'tt-pubkey') {
+              console.log('Public key:', event.data.payload.pubkey);
             }
             });
         `}
@@ -24,6 +38,18 @@ export default function DataForm() {
               data-packet-id="R1vaQ7Bebmx8N"
               data-referral-code="a4fc47faffd3cce77ea6df565fcbeaf6"
             />
+            {/* <div
+              id="tartle-container"
+              className="rounded-md overflow-hidden"
+              data-packet-id="oNpQVoKWjzXJN"
+              data-referral-code="2d454d17021543451c2747d2b9fdcd7f"
+            /> */}
+            {/* <div
+              id="tartle-container"
+              className="rounded-md overflow-hidden"
+              data-packet-id="WY7aQd95N6xvd"
+              data-referral-code="60c188d139d8555039bae931f49a0b59"
+            /> */}
           </div>
         </div>
       </div>
