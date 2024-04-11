22d0915b849276cdf0c01dbf7819c42e526b30e8
Gustavo Gallegos <goose@fractal.ly>
2024-04-10 19:30:00 -0700

    Change origins on complete page

--- src/app/tartle/completed/page.tsx
+++ src/app/tartle/completed/page.tsx
@@ -2,10 +2,26 @@
 import Head from 'next/head';
 import Script from 'next/script';
 
-export default function Completed() {
+const origins = {
+  production: 'https://source.tartle.co',
+  development: 'http://localhost:3000',
+  staging: 'https://staging.tartle.co'
+};
+
+export default function Completed({
+  searchParams
+}: {
+  searchParams: { env: 'production' | 'staging' | 'development' };
+}) {
+  const envParam = searchParams['env'];
+  let env: 'production' | 'staging' | 'development' = 'production';
+  if (envParam === 'staging' || envParam === 'development') {
+    env = envParam;
+  }
+
   return (
     <>
-      <Script src='https://source.tartle.co/scripts/endlink-completion.js' />
+      <Script src={`${origins[env]}/scripts/endlink-completion.js`} />
       <div className='mt-28 w-screen'>
         <div className='w-min mx-auto'>
           <div className='text-4xl tracking-tight font-extrabold   sm:text-5xl md:text-6xl lg:text-7xl'>
