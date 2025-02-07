5e88e1e7c3b306efb883c5213cc1c98a83700111
Gustavo Gallegos <goose@fractal.ly>
2025-02-07 12:03:35 -0800

    Add callback endpoint

--- src/app/api/tartle/route.ts
+++ src/app/api/tartle/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
-export async function GET() {
-  return NextResponse.json({ message: 'SUCCESS' }, { status: 200 });
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const params = Object.fromEntries(searchParams.entries());
+  return NextResponse.json({ url: request.url, params }, { status: 200 });
 }
