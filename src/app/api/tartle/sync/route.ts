307077525a780ebfcbf3ad13fdab8e5dcb078e50
Gustavo Gallegos <goose@fractal.ly>
2025-03-20 21:26:39 -0700

    better handling for sync

--- src/app/api/tartle/sync/route.ts
+++ src/app/api/tartle/sync/route.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from 'next/server';
 import { get } from '@vercel/edge-config';
+
 export const POST = async (request: Request) => {
   const { token, data } = await request.json();
 
   const config = (await get(process.env.EDGE_CONFIG_OBJECT_KEY as string)) as {
     packet_id: string;
   };
+
+  console.log({ config, token, data });
 
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_TARTLE_API_URI}/api/v5/packets/${config.packet_id}/sellers_packets/push`,
@@ -22,7 +25,16 @@ export const POST = async (request: Request) => {
     }
   );
 
-  const responseData = await response.json();
+  console.log({ response });
+  let responseData;
+  try {
+    responseData = await response.json();
+  } catch (error) {
+    console.error({ error });
+  }
+
+  console.log({ responseData });
+
   return NextResponse.json(responseData, {
     status: response.status,
     headers: response.headers
