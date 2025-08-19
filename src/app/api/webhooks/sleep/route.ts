b0427036c51dc2bcdb840dbcd4ac21dfa94aa02d
Gustavo Gallegos <goose@fractal.ly>
2025-08-19 16:11:28 -0700

    add webhook

---
+++ src/app/api/webhooks/sleep/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  //Grab the "p" url param
+  const { searchParams } = new URL(request.url);
+  const p = searchParams.get('p');
+
+  if (p !== process.env.SLEEP_PASSWORD) {
+    return NextResponse.json({ error: 'Invalid token' }, { status: 401 });
+  }
+
+  fetch(`https://webhooks.whpl.sh/sleep`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${process.env.WEBHOOKS_TOKEN}`,
+      'Content-Type': 'application/json'
+    }
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      return NextResponse.json(data, { status: 200 });
+    })
+    .catch((err) => {
+      return NextResponse.json({ error: err.message }, { status: 500 });
+    });
+}
