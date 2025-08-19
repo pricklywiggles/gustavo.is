2c0c5ddf6e54cd91473fbc44a5bbec61c12c9322
Gustavo Gallegos <goose@fractal.ly>
2025-08-19 16:26:10 -0700

    await call

--- src/app/api/webhooks/sleep/route.ts
+++ src/app/api/webhooks/sleep/route.ts
@@ -9,20 +9,21 @@ export async function GET(request: Request) {
     return NextResponse.json({ error: `Invalid token ${p}` }, { status: 401 });
   }
 
-  fetch(`https://webhooks.whpl.sh/sleep`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${process.env.WEBHOOKS_TOKEN}`,
-      'Content-Type': 'application/json'
-    }
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      return NextResponse.json(data, { status: 200 });
-    })
-    .catch((err) => {
-      return NextResponse.json({ error: err.message }, { status: 500 });
+  try {
+    const res = await fetch(`https://webhooks.whpl.sh/sleep`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.WEBHOOKS_TOKEN}`,
+        'Content-Type': 'application/json'
+      }
     });
+
+    if (!res.ok) {
+      return NextResponse.json({ error: 'Failed to sleep' }, { status: 500 });
+    }
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 500 });
+  }
 
   return NextResponse.json({ message: 'SUCCESS' }, { status: 200 });
 }
